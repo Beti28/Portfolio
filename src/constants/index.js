1b0b1203675a7fdfd7c00a080054d9423a794e4f
Beatris Ivanova <122818472+Beti28@users.noreply.github.com>
2025-01-04 00:31:59 +0200

    some fixes added

--- src/constants/index.js
+++ src/constants/index.js
@@ -125,12 +125,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Beti28',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/beatris-ivanova-517801339/',
     }
 ];
 
